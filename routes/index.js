d5723532ccb51aad967e6c531e7fa27295693899
Justin Womersley <justinwomersley@gmail.com>
2014-02-19 10:37:47 -0800

    Added basic swipe functionality

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ exports.view = function(req, res){
   // Load JSON data from static file
   var user_data = require("../user_data.json");
   var queue = user_data.queue;
+  var swipes = user_data.candidates;
 
   res.render('index', {
   	title: 'Rekindle',
-  	queue_results: queue
+  	queue_results: queue,
+  	candidates: swipes
   });
 };
