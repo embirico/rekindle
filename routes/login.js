bae4668161581936d105d1c815a966533f0b4ede
Justin Womersley <justinwomersley@gmail.com>
2014-03-12 06:32:18 -0700

    Improved Tour

--- routes/login.js
+++ routes/login.js
@@ -4,5 +4,12 @@
  */
 
 exports.view = function(req, res){
+
+  // If they haven't seen the tour yet, show them the tour
+  if(typeof req.cookies.seenTour == 'undefined') {
+  	res.cookie('seenTour', true);
+    res.redirect("/tour");
+  }
+
   res.render('login', {title: 'Rekindle'});
 };
